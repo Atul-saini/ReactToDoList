313d3abb2b6c3ffe72faeb304344f79049b7caa0
Atul Saini <62785658+Atul-saini@users.noreply.github.com>
2020-08-27 09:21:53 +0530

    Pop our done item we can pop item from list that is done.that happens when we click on it.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,56 @@
 import React, { useState } from "react";
-
+import ToDoItem from "./ToDoItem";
+import Footer from "./Footer";
 function App() {
-  const [inputtext, setinputtext] = useState("");
-  const [items, setitems] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
-  function handlechange(event) {
-    const newvalue = event.target.value;
-    setinputtext(newvalue);
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
 
-  function additem() {
-    setitems((prev) => [...prev, inputtext]);
-    setinputtext("");
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
   }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
+    <div>
+      <div className="container">
+        <div className="heading">
+          <h1>To-Do List</h1>
+        </div>
+        <div className="form">
+          <input onChange={handleChange} type="text" value={inputText} />
+          <button onClick={addItem}>
+            <span>Add</span>
+          </button>
+        </div>
+        <div>
+          <ul>
+            {items.map((todoItem, index) => (
+              <ToDoItem
+                key={index}
+                id={index}
+                text={todoItem}
+                onChecked={deleteItem}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="form">
-        <input onChange={handlechange} type="text" value={inputtext} />
-        <button onClick={additem}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {items.map((x) => (
-            <li> {x} </li>
-          ))}
-        </ul>
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Footer() {
+  return <h1 className="footer"> Mady By Atul</h1>;
+}
+export default Footer;
